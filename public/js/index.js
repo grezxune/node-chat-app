b081bc157b18d3c26d84f0b66a786a7e8add8637
tommy <tomtrezb2003@gmail.com>
2017-08-20 14:45:01 -0500

    added knockout and converted view to use it

--- public/js/index.js
+++ public/js/index.js
@@ -1,134 +1,118 @@
-  var socket = io();
-  var usersCurrentlyTyping = new Array();
-  var from = "";
-  var connectedUsers = new Array();
+function ViewModel() {
+    var self = this;
+    self.usersCurrentlyTyping = ko.observableArray();
+    self.name = ko.observable();
+    self.connectedUsers = ko.observableArray();
+    self.messages = ko.observableArray();
+    self.currentMessage = ko.observable('');
 
-  socket.on('connect', function() {
+    self.usersTypingMessage = ko.computed(function() {
+        var message = '';
+        var multipleUsersCurrentlyTyping = self.usersCurrentlyTyping().length > 1;
+
+        if(self.usersCurrentlyTyping().length > 0) {
+            message = self.usersCurrentlyTyping().join(', ') + (multipleUsersCurrentlyTyping ? ' have typed some shit' : ' has typed some shit');
+        }
+
+        return message;
+    });
+}
+
+function Message(from, text, createdAt) {
+    var self = this;
+    self.from = ko.observable(from);
+    self.text = ko.observable(text);
+    self.createdAt = ko.observable(createdAt);
+}
+
+var viewModel = new ViewModel();
+ko.applyBindings(viewModel, document.getElementById('master'));
+
+var socket = io();
+
+socket.on('connect', function () {
     console.log('connected to server');
-    while(!from || from.trim() === "") {
-       from = prompt("What is your name?");
+    while (!viewModel.name() || viewModel.name().trim() === "") {
+        viewModel.name(prompt("What is your name?"));
     }
 
-    $('#from').html(from);
     $('#text').focus();
+    socket.emit('newUserConnected', { user: viewModel.name() });
+});
 
-    socket.emit('newUserConnected', {user: from});
-  });
-
-  socket.on('disconnect', function() {
+socket.on('disconnect', function () {
     console.log('disconnected from server');
-  });
+});
 
-  socket.on('newMessage', function(message) {
+socket.on('newMessage', function (message) {
     console.log('new message', message);
-    addMessage(message);
-  });
+    viewModel.messages.push(new Message(message.from, message.text, message.createdAt));
+});
 
-  socket.on('connectedCountChanged', function(connection) {
+socket.on('connectedCountChanged', function (connection) {
     var oldCount = $('#connectionCount').html();
-    if(oldCount === "") {
-      oldCount = 0;
+    if (oldCount === "") {
+        oldCount = 0;
     }
 
-    addMessage("Connection count changed from " + oldCount + " to " + connection.connectedCount);
-    $('#connectionCount').html(connection.connectedCount);
-  });
+    // addMessage("Connection count changed from " + oldCount + " to " + connection.connectedCount);
+    // $('#connectionCount').html(connection.connectedCount);
+});
 
-  socket.on('isTyping', function(usersTyping) {
-    usersCurrentlyTyping = usersTyping.names;
-    var haveMultipleTyped = usersCurrentlyTyping.length > 1;
-    var message = usersCurrentlyTyping.join(', ') + (haveMultipleTyped ? ' have typed some shit' : ' has typed some shit');
-    $('#usersTyping').html(message);
-  });
+socket.on('isTyping', function (usersTyping) {
+    viewModel.usersCurrentlyTyping(usersTyping.names);
+});
 
-  socket.on('stoppedTyping', function(usersTyping) {
-    usersCurrentlyTyping = usersTyping.names;
-    var numberOfTypers = usersCurrentlyTyping.length;
-    var message = "";
+socket.on('stoppedTyping', function (usersTyping) {
+    viewModel.usersCurrentlyTyping(usersTyping.names);
+});
 
-    if(numberOfTypers > 0) {
-      var haveMultipleTyped = usersCurrentlyTyping.length > 1;
-      message = usersCurrentlyTyping.join(', ') + (haveMultipleTyped ? ' have typed some shit' : ' has typed some shit');
+socket.on('addUser', function (users) {
+    viewModel.connectedUsers(users.names);
+});
+
+socket.on('removeUser', function (users) {
+    viewModel.connectedUsers(users.names);
+});
+
+isTyping = function () {
+    if (viewModel.name().trim().length > 0) {
+        if (viewModel.currentMessage().trim().length > 0) {
+            socket.emit('isTyping', {
+                from: viewModel.name(),
+            });
+        } else {
+            socket.emit('stoppedTyping', {
+                from: viewModel.name()
+            });
+        }
     }
+}
 
-    $('#usersTyping').html(message);
-  });
+$(document).ready(function () {
+    $('#send').on('click', function () {
+        if (viewModel.name().trim().length > 0 && viewModel.currentMessage().trim().length > 0) {
+            socket.emit('createMessage', {
+                from: viewModel.name(),
+                text: viewModel.currentMessage()
+            });
 
-  socket.on('addUser', function(users) {
-    connectedUsers = users.names;
-    var usersHtml = "";
-
-    $.each(connectedUsers, function(index, value) {
-      usersHtml += "<span class=\"user-name\">" + value + "</span><br/>";
+            viewModel.currentMessage('');
+            $('#text').focus();
+            isTyping();
+        }
     });
 
-    $('.connected-users').html(usersHtml);
-  });
-
-  socket.on('removeUser', function(users) {
-    connectedUsers = users.names;
-    var usersHtml = "";
-
-    $.each(connectedUsers, function(index, value) {
-      usersHtml += "<span class=\"user-name\">" + value + "</span><br/>";
+    $('#text').on('keydown', function (key) {
+        if (key.keyCode === 13) {
+            key.preventDefault();
+            $('#send').click();
+        }
+    }).on('keyup', function (key) {
+        if (key.keyCode === 13) {
+            key.preventDefault();
+        } else {
+            isTyping();
+        }
     });
-
-    $('.connected-users').html(usersHtml);
-  });
-
-  createMessageText = function(message) {
-    return message.from + " (" + message.createdAt + "):\n" + message.text;
-  }
-
-  addMessage = function(message) {
-    var currentVal = $('#messages').val();
-    var newMessage = message.text ? createMessageText(message) : message;
-    $('#messages').val(currentVal + newMessage + "\n");
-    $('#messages').scrollTop($('#messages')[0].scrollHeight);
-  }
-
-  isTyping = function() {
-    var message = $('#text').val();
-
-    if(from.trim().length > 0) {
-      if(message.trim().length > 0) {
-        socket.emit('isTyping', {
-            from: from,
-        });
-      } else {
-        socket.emit('stoppedTyping', {
-          from: from
-        });
-      }
-    }
-  }
-
-$(document).ready(function() {
-  $('#send').on('click', function() {
-    var message = $('#text').val();
-
-    if(from.trim().length > 0 && message.trim().length > 0) {
-      socket.emit('createMessage', {
-        from: from,
-        text: message
-      });
-
-      $('#text').val('');
-      $('#text').focus();
-      isTyping();
-    }
-  });
-
-  $('#text').on('keydown', function(key) {
-    if(key.keyCode === 13) {
-      key.preventDefault();
-      $('#send').click();
-    }
-  }).on('keyup', function(key) {
-    if(key.keyCode === 13) {
-      key.preventDefault();
-    } else {
-      isTyping();
-    }
-  });
 });
--- server/server.js
+++ server/server.js
@@ -16,73 +16,73 @@ var usersConnected = new Array();
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-  console.log('new user connected');
-  connectedCount++;
-  io.emit('connectedCountChanged', { connectedCount: connectedCount });
-
-  // socket.emit('newMessage', {
-  //   from: 'tomtrezb2003@gmail.com',
-  //   text: 'hey man, what\'z up?',
-  //   createdAt: new Date().getTime()
-  // });
-
-  socket.on('createMessage', (newMessage) => {
-    console.log('createMessage', newMessage);
-
-    var date = new Date();
-    var options = {
-        weekday: "long", year: "numeric", month: "short",
-        day: "numeric", hour: "2-digit", minute: "2-digit", second: "2-digit"
-    };
-
-    io.emit('newMessage', {
-      from: newMessage.from,
-      text: newMessage.text,
-      createdAt: date.toLocaleDateString('en-us', options)
-    });
-  });
-
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-    connectedCount--;
+    console.log('new user connected');
+    connectedCount++;
     io.emit('connectedCountChanged', { connectedCount: connectedCount });
-    userStoppedTyping({from: socket.name});
-    var index = usersConnected.indexOf(socket.name);
-    usersConnected.splice(index, 1);
-    io.emit('removeUser', {names: usersConnected});
-  });
 
-  socket.on('isTyping', (user) => {
-    if(!usersTyping.includes(user.from)) {
-      usersTyping.push(user.from);
-      io.emit('isTyping', {names: usersTyping});
-    }
-  });
+    // socket.emit('newMessage', {
+    //   from: 'tomtrezb2003@gmail.com',
+    //   text: 'hey man, what\'z up?',
+    //   createdAt: new Date().getTime()
+    // });
 
-  socket.on('stoppedTyping', (user) => {
-    userStoppedTyping(user);
-  });
+    socket.on('createMessage', (newMessage) => {
+        console.log('createMessage', newMessage);
 
-  socket.on('newUserConnected', (user) => {
-    console.log('new user connected', user);
-    socket.name = user.user;
-    if(user.user && user.user.trim().length > 0) {
-      usersConnected.push(user.user);
-      io.emit('addUser', {names: usersConnected});
-    }
-  })
+        var date = new Date();
+        var options = {
+            weekday: "long", year: "numeric", month: "short",
+            day: "numeric", hour: "2-digit", minute: "2-digit", second: "2-digit"
+        };
+
+        io.emit('newMessage', {
+            from: newMessage.from,
+            text: newMessage.text,
+            createdAt: date.toLocaleDateString('en-us', options)
+        });
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+        connectedCount--;
+        io.emit('connectedCountChanged', { connectedCount: connectedCount });
+        userStoppedTyping({ from: socket.name });
+        var index = usersConnected.indexOf(socket.name);
+        usersConnected.splice(index, 1);
+        io.emit('removeUser', { names: usersConnected });
+    });
+
+    socket.on('isTyping', (user) => {
+        if (!usersTyping.includes(user.from)) {
+            usersTyping.push(user.from);
+            io.emit('isTyping', { names: usersTyping });
+        }
+    });
+
+    socket.on('stoppedTyping', (user) => {
+        userStoppedTyping(user);
+    });
+
+    socket.on('newUserConnected', (user) => {
+        console.log('new user connected', user);
+        socket.name = user.user;
+        if (user.user && user.user.trim().length > 0) {
+            usersConnected.push(user.user);
+            io.emit('addUser', { names: usersConnected });
+        }
+    })
 });
 
 userStoppedTyping = (user) => {
     console.log('userStoppedTyping', user);
-    if(usersTyping.includes(user.from)) {
-      var index = usersTyping.indexOf(user.from);
-      var newTest = usersTyping.splice(index, 1);
-      io.emit('stoppedTyping', {names: usersTyping});
+    if (usersTyping.includes(user.from)) {
+        var index = usersTyping.indexOf(user.from);
+        var newTest = usersTyping.splice(index, 1);
+        io.emit('stoppedTyping', { names: usersTyping });
     }
 };
 
 
 server.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+    console.log(`Listening on port ${port}`);
 });
